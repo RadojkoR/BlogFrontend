4546af04b453edd4b47aa1e5b6feadc8d8b59e7e
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-05-14 10:43:38 -0400

    added header in traveltips page

--- src/Pages/TravelTips.tsx
+++ src/Pages/TravelTips.tsx
@@ -1,7 +1,13 @@
+import { Header } from "../Components/Layout";
 
 
 function TravelTips() {
-  return <h1 className="my-20 text-center text-5xl">TravelTips</h1>;
+  return (
+    <>
+      <Header className="bg-bgHeaderBlog bg-contain bg-center" title="Travel Tips" />
+      <h1 className="my-20 text-center text-5xl">TravelTips</h1>
+    </>
+  );
 }
 
 export default TravelTips
